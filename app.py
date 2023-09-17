72e85ba35e92aeba776578710700f4b26533855f
Gaurav-822 <gaurav.moocs@gmail.com>
2023-09-17 10:21:23 +0530

    push

---
+++ app.py
@@ -0,0 +1,29 @@
+import os
+
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+
+# from helpers import apology
+
+# Configure Application
+app = Flask(__name__)
+
+# Ensure templates are auto reloded
+app.config["TEMPLATES_AUTO_RELOD"] = True
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/")
+def home():
+    return render_template('index.html')
