6f041db25ddb6840dc3f80bfddc7426139e20d2f
Gaurav-822 <gaurav.moocs@gmail.com>
2023-09-22 23:30:37 +0530

    .

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import os
 
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, Text, text, delete, insert
+from sqlalchemy.sql.expression import update, select
+
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 
-# from helpers import apology
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import apology, login_required
 
 # Configure Application
 app = Flask(__name__)
@@ -16,6 +21,24 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+# Making connection to the database
+engine = create_engine("sqlite:///data.db", echo = False, connect_args={"check_same_thread": False})
+conn = engine.connect()
+
+# Make Tables:
+meta = MetaData()
+users = Table(
+    'users', meta,
+    Column('id', Integer, primary_key = True),
+    Column('username', Text),
+    Column('hash', Text),
+    Column('password', Text),
+    Column('phone', Text),
+    Column('aadhaar', Text)
+)
+
+meta.create_all(engine)
+
 @app.after_request
 def after_request(response):
     """Ensure responses aren't cached"""
@@ -25,5 +48,67 @@ def after_request(response):
     return response
 
 @app.route("/")
-def home():
+@login_required
+def index():
+    if session['user_id']:
+        return render_template('index.html')
+    return render_template('login.html')
+
+# Login and Register
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    # Forget any user id
+    session.clear()
+
+    if request.method == 'GET':
+        return render_template('register.html')
+    
+    # Data at the time of register
+    username = request.form.get('username')
+    password = request.form.get('password')
+    re_password = request.form.get('re_password')
+    if password != re_password:
+        return render_template('apology.html', 'Passwords don\'t match')
+    phone = request.form.get('phone')
+
+    user = text('SELECT phone FROM users GROUP BY id')
+    result = conn.execute(user)
+    res = []
+    for row in result:
+        res.append(row[0])
+    if phone in res:
+        return render_template('apology.html', txt='This phone no. is already registered!, Try to login with this no. of register with another no.')
+    
+    ins = users.insert().values(username = username, hash = generate_password_hash(password), phone = phone, aadhaar = 'Na')
+    conn.execute(ins)
+
+    session['user_id'] = id
+
     return render_template('index.html')
+
+# Login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # Forget any user id
+    session.clear()
+
+    if request.method == 'GET':
+        return render_template('login.html')
+    
+    # Data at the time of register
+    username = request.form.get('username')
+    password = request.form.get('password')
+    
+    user = text('SELECT username, hash, id FROM users')
+    result = conn.execute(user)
+    for row in result:
+        if row[0] == username and check_password_hash(row[1], password):
+            session['user_id'] = row[2]
+            return render_template('index.html')
+    
+    return render_template('apology.html', txt = 'Login Error!')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
